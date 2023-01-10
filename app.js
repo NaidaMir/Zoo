7d14111b78af4e2250c844556bc503d0f5fb4196
Naida <satana_dance@mail.ru>
2023-01-10 13:49:16 +0500

    init, add sessions

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
+require('@babel/register');
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 const { sequelize } = require('./db/models');
 
 const app = express();
 const morgan = require('morgan');
 
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 const { PORT, SESSION_SECRET } = process.env;
 
+app.use(express.static(path.resolve('public')));
 app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const sessionConfig = {
+  name: 'LeopardsCookie',
+  store: new FileStore(),
+  secret: SESSION_SECRET ?? 'zoo',
+  resave: false, 
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 9999999,
+    httpOnly: true,
+  }
+};
+
+app.use(session(sessionConfig));
 
 app.get('/', (req, res) => {
   res.send('Привет!');
